6b162fe62168fff6493ff967febfaa4bbeb8909b
seiko <seiko_des@outlook.com>
2022-08-22 16:36:24 +0800

    add ios simulator

--- app/ios/build.gradle.kts
+++ app/ios/build.gradle.kts
@@ -10,6 +10,7 @@ kotlin {
                 entryPoint = "com.seiko.compose.hellorust.demo.main"
                 freeCompilerArgs = freeCompilerArgs + listOf(
                     "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-lresolv",
                 )
             }
         }
@@ -26,6 +27,16 @@ kotlin {
 compose.experimental {
     uikit.application {
         bundleIdPrefix = "com.seiko.compose.hellorust.demo"
-        projectName = "Compose Vlc"
+        projectName = "ComposeHelloRust"
+        deployConfigurations {
+            simulator("Simulator") {
+                device = org.jetbrains.compose.experimental.dsl.IOSDevices.IPHONE_13_MINI
+            }
+        }
     }
+}
+
+val runIos by tasks.registering {
+    group = "run"
+    dependsOn("iosDeploySimulatorDebug")
 }
